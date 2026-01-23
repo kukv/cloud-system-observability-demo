cf855ec35ddcf0dd687e55eedb7daa2c26b6910b
kukv <koki-nonaka@outlook.jp>
2026-01-23 23:47:34 +0900

    init

---
+++ src/main/java/jp/kukv/cloudsystemobservabilitydemo/CloudSystemObservabilityDemoApplication.java
@@ -0,0 +1,13 @@
+package jp.kukv.cloudsystemobservabilitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSystemObservabilityDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloudSystemObservabilityDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/jp/kukv/cloudsystemobservabilitydemo/CloudSystemObservabilityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kukv.cloudsystemobservabilitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSystemObservabilityDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
